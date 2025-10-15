4e1039f9f59f230afe104d1127904ccc9da611ab
Marko <markgufman21@gmail.com>
2025-10-16 01:14:54 +0200

    Enhance Hero component text layout by adding line breaks for better readability and adjusting button styles for improved consistency

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -21,8 +21,8 @@ export default function Hero() {
         />
         <div className="z-10 flex flex-col items-center gap-8 justify-center px-12 py-[25%]">
           <div className="flex flex-col gap-4 z-10">
-            <div className="text-white text-4xl max-w-xl  text-center font-garamond font-bold uppercase">
-              Köstliches Essen & ein wundervolles Genusserlebnis
+            <div className="text-white text-4xl w-full max-w-xl text-center font-garamond font-bold uppercase">
+              Köstliches <br /> Essen & ein wundervolles Genusserlebnis
             </div>
             <div className="text-white text-md text-center font-crimson">
               Inspiriert von Reisen und der Liebe zu verschiedenen Kulturen
@@ -30,11 +30,11 @@ export default function Hero() {
               Zutaten – komm vorbei und genieße sie!
             </div>
           </div>
-          <div className="flex z-10 flex-col items-center justify-center gap-4">
-            <button className="bg-[#F5C84F] font-crimson  text-[#193039] px-8 py-4 uppercase font-bold">
+          <div className="flex z-10 flex-col items-center justify-center gap-4 w-full max-w-xl">
+            <button className="bg-[#F5C84F] font-crimson text-[#193039] w-full py-4 uppercase font-bold border-2 border-transparent">
               Unsere Speisekarte
             </button>
-            <button className="border-2 border-white text-white px-8 py-4 uppercase font-semibold">
+            <button className="border-2 border-white text-white font-crimson w-full py-4 uppercase font-bold">
               Online bestellen
             </button>
           </div>
