f8fbd7a552f318670b0e60469ec8ce3e3e580831
Marko <markgufman21@gmail.com>
2025-10-16 00:31:59 +0200

    Add Feature component to homepage for enhanced functionality

---
+++ src/app/components/feature.tsx
@@ -0,0 +1,7 @@
+export default function Feature() {
+  return (
+    <div className="w-full h-screen bg-white">
+      <div>Feature</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Feature from "./components/feature";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Feature />
     </div>
   );
 }
