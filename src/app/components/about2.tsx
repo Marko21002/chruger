6957c81233250a69b5c877d8d1b459ce48993620
Marko <markgufman21@gmail.com>
2025-10-15 23:36:16 +0200

    Refactor About2 component to use Next.js Image component for optimized image loading and update text for proper HTML entity encoding

--- src/app/components/about2.tsx
+++ src/app/components/about2.tsx
@@ -1,30 +1,40 @@
+import Image from "next/image";
+
 export default function About2() {
   return (
     <div className="w-full h-auto pt-12">
       <div className="w-full h-screen bg-[#04242C] relative flex flex-col items-center pt-32 px-4">
-        <img
+        <Image
           src="/about.png"
           alt="about2"
-          className="w-full z-0 h-full absolute top-0 left-0 object-cover opacity-60"
+          layout="fill"
+          objectFit="cover"
+          className="z-0 opacity-60"
         />
         <div className="flex items-center justify-center gap-4 z-10 text-white">
           <div>
-            <img src="/path2.png" alt="path" />
+            <Image src="/path2.png" alt="path" width={64} height={64} />
           </div>
           <div className="text-md font-bold uppercase text-[#EFC9A4]">
             UNSERE GESCHICHTE
           </div>
           <div>
-            <img src="/path2.png" alt="path" className="w-16" />
+            <Image
+              src="/path2.png"
+              alt="path"
+              width={64}
+              height={64}
+              className="w-16"
+            />
           </div>
         </div>
 
         <div className="z-10 text-white text-center mt-8 flex flex-col items-center gap-8">
           <h1 className="text-4xl font-bold max-w-lg">
-            CHRUGER'S NO.10: EINE GEMÜTLICHE ECKE FÜR FEINSCHMECKER IN WIEN
+            CHRUGER&apos;S NO.10: EINE GEMÜTLICHE ECKE FÜR FEINSCHMECKER IN WIEN
           </h1>
           <p className="max-w-xl text-sm opacity-80">
-            Chruger's No.10 wurde 2021 in der historischen Krugerstraße 10
+            Chruger&apos;s No.10 wurde 2021 in der historischen Krugerstraße 10
             gegründet – mitten im lebendigen Herzen Wiens. Das gemütliche
             Interieur in zentraler Lage, kombiniert mit unserer Leidenschaft für
             marktfrische Zutaten, ehrliche Küche und eine einladende Atmosphäre,
@@ -34,8 +44,8 @@ export default function About2() {
             entspannte Momente sind. Mit unserer sorgfältig zusammengestellten
             Auswahl an köstlichen Kombinationen ist für jeden Geschmack etwas
             dabei. Sowohl Einheimische als auch reisende Feinschmecker lieben
-            die Atmosphäre und Küche im Chruger's No.10. Wir freuen uns darauf,
-            Sie bei uns willkommen zu heißen!
+            die Atmosphäre und Küche im Chruger&apos;s No.10. Wir freuen uns
+            darauf, Sie bei uns willkommen zu heißen!
           </p>
           <button className="bg-[#F5C84F] text-black font-bold py-4 px-12  mt-4">
             KONTAKT
