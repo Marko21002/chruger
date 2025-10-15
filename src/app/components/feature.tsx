3fd218c4da6b88817d47e1e270fb4f4971131a03
Marko <markgufman21@gmail.com>
2025-10-16 00:52:32 +0200

    Add Cormorant Garamond and Poppins fonts to layout; enhance Feature component structure; update Hero component font styling

--- src/app/components/feature.tsx
+++ src/app/components/feature.tsx
@@ -1,3 +1,11 @@
 export default function Feature() {
-  return <div className="w-full h-screen bg-white"></div>;
+  return (
+    <div className="w-full h-screen bg-white">
+      <div className="w-full pt-[15vh]">
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="text-4xl font-bold">Feature</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Cormorant_Garamond,
+  Poppins,
+} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +15,17 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const cormorantGaramond = Cormorant_Garamond({
+  variable: "--font-cormorant-garamond",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${cormorantGaramond.variable} ${poppins.variable} ${poppins.className} ${cormorantGaramond.className} antialiased`}
       >
         {children}
       </body>
