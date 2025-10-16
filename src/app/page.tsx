802400a4e220120f30dd5fd13d9a74d6e9e566ed
Marko <markgufman21@gmail.com>
2025-10-16 15:29:56 +0200

    Add Feature component to Home page for enhanced content presentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Frame from "./components/test";
+import Feature from "./components/feature";
+import About from "./components/about";
 
 export default function Home() {
   return (
     <div>
       <Frame />
+      <Feature />
     </div>
   );
 }
