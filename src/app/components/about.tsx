6534075fde896fe06dcd51ff232b4da26314c276
Marko <markgufman21@gmail.com>
2025-10-16 15:40:59 +0200

    Add About component to Home page and update About component styles for improved layout and content presentation

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -2,36 +2,43 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="w-full h-fit bg-[#04242C] relative px-4 pb-12 pt-12">
+    <div className="relative bg-[#04242C] py-24 px-6">
       <Image
-        src="/about.png"
-        alt="about"
-        width={1000}
-        height={1000}
-        className="w-full h-full z-0 object-cover opacity-65 absolute top-0 left-0"
+        src="https://workers.paper.design/file-assets/01K7PE35P5BXHACJRK07P8DF7H/01K7PKKZW66DWYSCYGRE884BYY.png"
+        alt="background"
+        layout="fill"
+        objectFit="cover"
+        className="z-0"
       />
-      <div className="flex flex-col items-center justify-center gap-8">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <div className="text-white text-center z-10 text-4xl font-garamond font-semibold uppercase">
-            Chruger’s No.10: Eine gemütliche Ecke für Feinschmecker in Wien
-          </div>
-          <div className="text-white text-center z-10 text-md font-crimson opacity-80">
-            Chruger’s No.10 wurde 2021 in der historischen Krugerstraße 10
-            gegründet – mitten im lebendigen Herzen Wiens. Das gemütliche
-            Interieur in zentraler Lage, kombiniert mit unserer Leidenschaft für
-            marktfrische Zutaten, ehrliche Küche und eine einladende Atmosphäre,
-            prägt unsere Identität. Unsere Speisekarte bietet eine Vielzahl an
-            Gerichten – von Tacos, Bowls und Burritos bis hin zu Salaten und
-            Desserts – begleitet von ausgewählten Weinen, die perfekt für
-            entspannte Momente sind. Mit unserer sorgfältig zusammengestellten
-            Auswahl an köstlichen Kombinationen ist für jeden Geschmack etwas
-            dabei. Sowohl Einheimische als auch reisende Feinschmecker lieben
-            die Atmosphäre und Küche im Chruger’s No.10.Wir freuen uns darauf,
-            Sie bei uns willkommen zu heißen!
+      <div className="relative z-10 flex flex-col items-center gap-10 text-center">
+        <div className="flex flex-col items-center gap-5 text-white">
+          <h2 className="max-w-md font-garamond text-4xl font-bold uppercase">
+            CHRUGER’S NO.10: EINE GEMÜTLICHE ECKE FÜR FEINSCHMECKER IN WIEN
+          </h2>
+          <div className="flex flex-col gap-4 max-w-sm font-crimson text-sm leading-relaxed text-white/80">
+            <p>
+              Chruger’s No.10 wurde 2021 in der historischen Krugerstraße 10
+              gegründet – mitten im lebendigen Herzen Wiens. Das gemütliche
+              Interieur in zentraler Lage, kombiniert mit unserer Leidenschaft
+              für marktfrische Zutaten, ehrliche Küche und eine einladende
+              Atmosphäre, prägt unsere Identität.
+            </p>
+            <p>
+              Unsere Speisekarte bietet eine Vielzahl an Gerichten – von Tacos,
+              Bowls und Burritos bis hin zu Salaten und Desserts – begleitet von
+              ausgewählten Weinen, die perfekt für entspannte Momente sind.
+            </p>
+            <p>
+              Mit unserer sorgfältig zusammengestellten Auswahl an köstlichen
+              Kombinationen ist für jeden Geschmack etwas dabei. Sowohl
+              Einheimische als auch reisende Feinschmecker lieben die Atmosphäre
+              und Küche im Chruger’s No.10. Wir freuen uns darauf, Sie bei uns
+              willkommen zu heißen!
+            </p>
           </div>
         </div>
-        <button className="bg-[#F5C84F] z-10 uppercase font-crimson text-[#193039] w-full py-4 text-center font-bold border-2 border-transparent">
-          Kontakt
+        <button className="bg-[#F5C84F] px-5 py-3 font-crimson text-base font-semibold uppercase text-black">
+          KONTAKT
         </button>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <div>
       <Frame />
       <Feature />
+      <About />
     </div>
   );
 }
